2d392b87bd3061e897bcbaf456e08639f737bf3d
jaydenweaver <jayden_weaver@hotmail.com>
2025-08-24 23:57:01 +1000

    texture loader

--- src/loader.rs
+++ src/loader.rs
@@ -1,8 +1,18 @@
-use crate::asset::Asset;
+use crate::asset::*;
 use std::path::Path;
 
 pub trait AssetLoader: Send + Sync + 'static {
     type AssetType: Asset;
 
     fn load(&self, path: &Path) -> Result<Self::AssetType, String>;
+}
+
+pub struct TextureLoader;
+
+impl AssetLoader for TextureLoader {
+    type AssetType = Texture;
+
+    fn load(&self, path: &Path) -> Result<Self::AssetType, String> {
+        Ok(format!("loaded texture from {:?}", path))
+    }
 }
